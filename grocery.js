2578fbaf9cc896b4025fa3e2e77cb046618a45e5
LeAnna <leanna.nicole@yahoo.com>
2024-03-06 12:26:18 -0800

    adding files

--- grocery.js
+++ grocery.js
@@ -1,1 +1,16 @@
-console.log("Hello World")
+const body = document.getElementsByTagName("body")
+const groceryList = document.createElement("div")
+append(groceryList)
+groceryList.append (groceryHeader)
+const groceryHeader = document.createElement("h1")
+groceryHeader.textContent = ("groceryList")
+const groceryFridge = document.querySelector("h2")
+groceryFridge.append(groceryList)
+const ul = document.createElement("ul")
+const liOne = document.createElement("ul")
+liOne.textContent = "Milk"
+const liTwo = document.createElement("ul")
+liTwo.textContent = "Cheese"
+const liThree = document.createElement("ul")
+liThree.textContent = "Butter"
+ul.append (liOne, liTwo, liThree)
